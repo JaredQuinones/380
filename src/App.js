d16cd4976c8ea39e1eced03a0ef4ccd4d1103016
JaredQuinones <127901195+JaredQuinones@users.noreply.github.com>
2023-03-21 20:12:21 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -4,11 +4,18 @@ import Bio from './Bio';
 import Bio2 from './Bio2';
 import MyCarousel from './MyCarousel';
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Routes, Route, Outlet, Link } from "react-router-dom";
+import { Carousel } from 'bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <MyCarousel />
+      <Link to="/">Home</Link>
+      <Link to="/carousel">Carousel</Link>
+      <Routes>
+        <Route path="/" element={<Bio2/>}></Route>
+        <Route path="carousel" element={<MyCarousel/>}></Route>
+      </Routes>
     </div>
   );
 }
